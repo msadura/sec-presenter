89ef42b48b31b5fe23c8fe42f7194e49de4c9946
Marek Sadura <m.sadura@outlook.com>
2019-05-30 18:14:00 +0200

    Material UI

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React,  { Component } from 'react';
+import Fullscreen from "react-full-screen";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      isFull: false,
+    };
+  }
+
+  goFull = () => {
+    this.setState({ isFull: true });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.goFull}>
+          Go Fullscreen
+          </button>
+  
+        <Fullscreen
+          enabled={this.state.isFull}
+          onChange={isFull => this.setState({ isFull })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+          </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+          </a>
+          </header>
+        </Fullscreen>
+      </div>
+    );
+  }
 }
 
 export default App;
