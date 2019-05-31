46eff4217ff5160320ae553efe4ef7dc586f8566
Marek Sadura <m.sadura@outlook.com>
2019-05-31 22:05:25 +0200

    Updates

---
+++ precache-manifest.48f9e740b22b537e801f52859f6fba08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b62c22d94be1d9d7c1ff61e5cea9c3",
+    "url": "/sec-presenter/index.html"
+  },
+  {
+    "revision": "0fca4a4465fbb26f2f42",
+    "url": "/sec-presenter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6ff7f6c9e3155f76d9b8",
+    "url": "/sec-presenter/static/js/2.d6c8272e.chunk.js"
+  },
+  {
+    "revision": "0fca4a4465fbb26f2f42",
+    "url": "/sec-presenter/static/js/main.f717ae2c.chunk.js"
+  },
+  {
+    "revision": "02679b1d08f613434658",
+    "url": "/sec-presenter/static/js/runtime~main.29298901.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sec-presenter/static/media/logo.5d5d9eef.svg"
+  }
+]);
